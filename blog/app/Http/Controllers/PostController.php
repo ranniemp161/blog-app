448f7f07ea6c3eaf938942d1869bd40a57543e88
rannieperalta <rannieperalta06@gmail.com>
2024-09-26 19:03:27 +0800

    I added a middleware and the abilitiy to post

---
+++ blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //show the post form:
+    public function showPostForm()
+    {
+        return view('create-post');
+    }
+
+    //creating actual post:
+    public function storeNewPost(Request $request)
+    {
+        $userData = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $userData['title'] = strip_tags($userData['title']);
+        $userData['body'] = strip_tags($userData['body']);
+
+        $userData['user_id'] = auth()->id();
+
+        $newPost = Post::create($userData);
+
+        return redirect("/post/{$newPost->id}")->with('success', 'New post created');
+    }
+
+    // Show the single post:
+    public function viewSinglePost(Post $post)
+    {
+        $post['body'] = Str::markdown($post->body);
+
+        return view('single-post', ['post' => $post]);
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//User Related routes:
+Route::get('/', [UserController::class, "homefeed"])->name('login');
+Route::post('/register', [UserController::class, "register"])->middleware('guest');
+Route::post('/login', [UserController::class, "login"])->middleware('guest');
+Route::post('/logout', [UserController::class, "logout"])->middleware('auth');
 
-Route::get('/', [UserController::class, "homefeed"]);
-
-Route::post('/register', [UserController::class, "register"]);
-Route::post('/login', [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, "logout"]);
+// Blog post related routes:
+Route::get('/create-post', [PostController::class, 'showPostForm'])->middleware('shouldBeloggedIn');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('shouldBeloggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->middleware('shouldBeloggedIn');
